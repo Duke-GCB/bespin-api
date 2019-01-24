59c08bece36001c922ed0f3f4d83d70f3b30cd38
John Bradley <johnbradley2008@gmail.com>
2019-01-24 11:20:17 -0500

    add organize output project step

--- data/models.py
+++ data/models.py
@@ -189,6 +189,7 @@ class Job(models.Model):
     JOB_STEP_CREATE_VM = 'V'
     JOB_STEP_STAGING = 'S'
     JOB_STEP_RUNNING = 'R'
+    JOB_STEP_ORGANIZE_OUTPUT_PROJECT = 'o'
     JOB_STEP_STORE_OUTPUT = 'O'
     JOB_STEP_RECORD_OUTPUT_PROJECT = 'P'
     JOB_STEP_TERMINATE_VM = 'T'
@@ -196,6 +197,7 @@ class Job(models.Model):
         (JOB_STEP_CREATE_VM, 'Create VM'),
         (JOB_STEP_STAGING, 'Staging In'),
         (JOB_STEP_RUNNING, 'Running Workflow'),
+        (JOB_STEP_ORGANIZE_OUTPUT_PROJECT, 'Organize Output Project'),
         (JOB_STEP_STORE_OUTPUT, 'Store Job Output'),
         (JOB_STEP_RECORD_OUTPUT_PROJECT, 'Record Output Project'),
         (JOB_STEP_TERMINATE_VM, 'Terminate VM'),
